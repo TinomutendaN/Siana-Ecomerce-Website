2e734a3332a41ab871ceb9e5a5a45f1bf13d3276
Tinomutenda <tynoenyamhingura@gmail.com>
2024-08-16 17:12:58 +0200

    register and product page

--- src/store/urls.py
+++ src/store/urls.py
@@ -6,5 +6,7 @@ urlpatterns = [
     path("", views.index, name = "index"),
     path("login/", views.login_user, name = "login"),
     path("logout/", views.logout_user, name = "logout"),
+    path("register/", views.register_user, name = "register"),
+    path("product/<int:pk>/", views.product, name="product"),
     
    ]
--- src/store/views.py
+++ src/store/views.py
@@ -2,7 +2,10 @@ from django.shortcuts import render, redirect
 from .models import Product
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages 
-
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
+from .forms import SignUpForm
+from django import forms 
 
 
 # Create your views here.
@@ -31,4 +34,41 @@ def logout_user(request):
     logout(request)
     messages.success(request,("You have been logged out...."))
     return redirect('index')
+
+def register_user(request):
+       form = SignUpForm()
+       if request.method == "POST":
+           form = SignUpForm(request.POST)
+           if form.is_valid():
+               form.save()
+               username = form.cleaned_data['username']
+               password = form.cleaned_data['password1']
+
+               # login user
+               user = authenticate(username=username, password=password)
+               login(request, user)
+               messages.success(request,("You have registered successfully !!!"))
+               return redirect('index')
+           else:
+               messages.success(request,("Oops there was a problem registering, please try again later"))
+               return redirect('register')
+
+       else:
+            return render(request, 'register.html',{'form':form})
+       
+
+#def product(request,pk):
+    #product = Product.objects.get(id=pk)
+    #return render(request, 'product.html',{'products':product})
+
+def product(request, pk):
+    try:
+        product = Product.objects.get(id=pk)
+        return render(request, 'product.html', {'product': product})
+    except Product.DoesNotExist:
+        return redirect('index')  # Redirect to the homepage or an error page if the product doesn't exist
+
+
+
+
 
