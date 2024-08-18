b267e4f90366d7c4a5f707e50280325c6f421872
Tinomutenda <tynoenyamhingura@gmail.com>
2024-08-18 03:27:30 +0200

    shopping cart added

--- src/store/views.py
+++ src/store/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect
-from .models import Product
+from .models import Product, Category
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages 
 from django.contrib.auth.models import User
@@ -11,7 +11,8 @@ from django import forms
 # Create your views here.
 def index(request):
     products = Product.objects.all()
-    return render(request, 'index.html',{'products':products})
+    categories = Category.objects.all()
+    return render(request, 'index.html', {'products': products, 'categories': categories})
 
 def login_user(request):
     if request.method == "POST":
@@ -61,12 +62,49 @@ def register_user(request):
     #product = Product.objects.get(id=pk)
     #return render(request, 'product.html',{'products':product})
 
+#def product(request, pk):
+    #try:
+        #product = Product.objects.get(id=pk)
+        #return render(request, 'product.html', {'product': product})
+    #except Product.DoesNotExist:
+        #return redirect('index')  # Redirect to the homepage or an error page if the product doesn't exist
+
+
 def product(request, pk):
     try:
         product = Product.objects.get(id=pk)
-        return render(request, 'product.html', {'product': product})
+        related_products = Product.objects.filter(category=product.category).exclude(id=pk)[:4]  # Get related products, limit to 4
+        return render(request, 'product.html', {'product': product, 'related_products': related_products})
     except Product.DoesNotExist:
-        return redirect('index')  # Redirect to the homepage or an error page if the product doesn't exist
+        return redirect('index')  # Redirect if the product doesn't exist
+    
+#def category(request,foo):
+    # Replace Hyphens with Spaces
+    #foo = foo.replace('-', ' ')
+    # Grab the category from the url
+    #try:
+        # look up the category
+        #category = Category.objects.get(name=foo)
+        #products = Product.objects.filter(category=category)
+        #return render(request, 'category.html', {'products':products, 'category':category})
+
+
+    #except:
+        #return redirect('index')
+
+def category(request, foo):
+    # Replace hyphens with spaces to match category names in the database
+    foo = foo.replace('-', ' ')
+    
+    try:
+        # Look up the category
+        category = Category.objects.get(name=foo)
+        products = Product.objects.filter(category=category)
+        return render(request, 'category.html', {'products': products, 'category': category})
+    
+    except Category.DoesNotExist:
+        return redirect('index')  # Redirect to the homepage if the category doesn't exist
+
 
 
 
