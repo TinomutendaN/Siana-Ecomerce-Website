6fcf2dfbef809da72820432925ebe02b1bdaa204
Tinomutenda <tynoenyamhingura@gmail.com>
2024-08-25 21:21:11 +0200

    settings

--- src/Siana/settings.py
+++ src/Siana/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = "django-insecure-4+$w58(2opt2$j3j6j=_g7*a58jslfl5(%dkd%qq=$30yrqq*v
 DEBUG = True
 
 ALLOWED_HOSTS = ["siana-ecomerce-website-production.up.railway.app", "https://siana-ecomerce-website-production.up.railway.app"]
-CSRF_TRUSTED_ORIGINS = ["siana-ecomerce-website-production.up.railway.app", "https://siana-ecomerce-website-production.up.railway.app"]
+CSRF_TRUSTED_ORIGINS = ["https://siana-ecomerce-website-production.up.railway.app"]
 
 
 # Application definition
