5f58178b685b34d8c535c0ebfbe1bb3f284f522f
Tinomutenda <tynoenyamhingura@gmail.com>
2024-08-26 04:39:09 +0200

    login settings

--- src/Siana/settings.py
+++ src/Siana/settings.py
@@ -155,3 +155,6 @@ STATIC_ROOT = BASE_DIR / "staticfiles"
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+# settings.py
+LOGIN_URL = 'login'  # or '/login/' if you prefer the full path
